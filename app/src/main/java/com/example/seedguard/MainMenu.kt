e47df999e0c29a02ba9990f4c3dc166c66d624f0
Jakob <jakob.landless@students.plymouth.ac.uk>
2025-04-09 19:37:03 +0100

    The option on the main menu for enabling SMS 2FA is now hidden if the user is already enrolled with a phone number.
    To do this, I simply added a new function which would make a request to Firebase Auth, retrieving the user's enrolled factors and checking to see whether they were enrolled with a phone number or not. If a phone number is enrolled, the button is hidden, and if not then the user can click on it as normal.

--- app/src/main/java/com/example/seedguard/MainMenu.kt
+++ app/src/main/java/com/example/seedguard/MainMenu.kt
@@ -4,38 +4,69 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.PhoneMultiFactorInfo
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainMenu : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var enableSmsButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
 
+        auth = Firebase.auth
+        enableSmsButton = findViewById(R.id.button3)
+
         val viewSavedButton: Button = findViewById(R.id.button)
         val addNewButton: Button = findViewById(R.id.button2)
-
+        val deleteAccountButton: Button = findViewById(R.id.button4)
 
         viewSavedButton.setOnClickListener {
-            val intent = Intent(this, ViewWalletsActivity::class.java)
-            startActivity(intent)
+            startActivity(Intent(this, ViewWalletsActivity::class.java))
         }
 
         addNewButton.setOnClickListener {
-            val intent = Intent(this, AddNewSeedPhraseActivity::class.java)
-            startActivity(intent)
+            startActivity(Intent(this, AddNewSeedPhraseActivity::class.java))
         }
-        val enableSmsButton: Button = findViewById(R.id.button3)
 
-        enableSmsButton.setOnClickListener {
-            val intent = Intent(this, PhoneVerificationActivity::class.java)
-            startActivity(intent)
-        }
-        val deleteAccountButton: Button = findViewById(R.id.button4)
         deleteAccountButton.setOnClickListener {
-            val intent = Intent(this, DeleteAccountActivity::class.java)
-            startActivity(intent)
+            startActivity(Intent(this, DeleteAccountActivity::class.java))
         }
+    }
 
+    override fun onStart() {
+        super.onStart()
+        checkSmsEnrollmentStatus(auth.currentUser)
+    }
+
+    private fun checkSmsEnrollmentStatus(user: FirebaseUser?) {
+        user?.let {
+            if (it.multiFactor.enrolledFactors.isNotEmpty()) {
+                for (factor in it.multiFactor.enrolledFactors) {
+                    if (factor is PhoneMultiFactorInfo) {
+                        enableSmsButton.visibility = Button.GONE
+                    } else {
+                        enableSmsButton.visibility = Button.VISIBLE
+                        enableSmsButton.setOnClickListener {
+                            startActivity(Intent(this, PhoneVerificationActivity::class.java))
+                        }
+                    }
+                }
+            } else {
+                enableSmsButton.visibility = Button.VISIBLE
+                enableSmsButton.setOnClickListener {
+                    startActivity(Intent(this, PhoneVerificationActivity::class.java))
+                }
+
+
+            }
+        } ?: run {
+            enableSmsButton.visibility = Button.VISIBLE
+        }
     }
 }
